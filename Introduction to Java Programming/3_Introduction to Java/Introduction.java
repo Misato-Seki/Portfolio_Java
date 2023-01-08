98df11fdf0c5095e40ffd054376a912c8cb8ffc0
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2023-01-08 17:18:23 +0200

    add new file

---
+++ Introduction to Java Programming/3_Introduction to Java/Introduction.java
@@ -0,0 +1,6 @@
+public class Introduction 
+{
+  public static void main (String [] args) {
+    System.out.println("First contact with Java-programming.");
+  }
+}
---
+++ Introduction to Java Programming/4_Types, variables, statements & blocks/Concatenation.java
@@ -0,0 +1,18 @@
+public class Concatenation {
+    public static void main(String[] args) {
+        int first = 1;
+		int second = 2;
+		int third = 7;
+		String fourth = "9";
+		String fifth = "5";
+
+		System.out.println(first + fourth + (second + third) + fifth
+             + ":Finland was the world champion in ice hockey.");
+                        
+        System.out.println(first + "" + (first + third) + (first - 1) + fourth
+             + ": Finland went under Russian rule.");   
+                       
+        System.out.println(first + fifth + fifth + (first - 1)
+             + ": Helsinki was founded.");
+    }
+}
